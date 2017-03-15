08c37eba8b44d251f09dfb6541bfadc589cb67e0
Rick Riehle <rickr@bitbamboo.com>
2017-03-14 22:25:41 -0700

    Updating presentation materials

--- build/html/searchindex.js
+++ build/html/searchindex.js
@@ -1,1 +1,1 @@
-Search.setIndex({docnames:["index"],envversion:50,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to Py300&#8217;s documentation!"],titleterms:{document:0,indic:0,py300:0,tabl:0,welcom:0}})
+Search.setIndex({docnames:["index"],envversion:50,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,page:0,search:0},titles:["Welcome to Py100"],titleterms:{indic:0,modul:0,py100:0,tabl:0,welcom:0}})

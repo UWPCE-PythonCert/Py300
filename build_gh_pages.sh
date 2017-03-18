64154b00b2496b39d347be4d0ec803aee73f1139
Chris Barker <PythonCHB@gmail.com>
2017-03-18 14:46:59 -0700

    updated build_gh_pages.sh

--- build_gh_pages.sh
+++ build_gh_pages.sh
@@ -3,11 +3,31 @@
 # simple script to build and push to gh-pages
 # designed to be run from master
 
+# this version used a separate copy of the repo in a parallel dir
+# to keep the gh-pages branch nicely separate from the main branch.
+
+GHPAGESDIR=../Py300.gh-pages/
+
+# make sure dir is there:
+if [ ! -d $GHPAGESDIR ]; then
+    echo "To build the gitHub pages, you must first create a parallel repo: $GHPAGESDIR"
+    exit
+fi
+
+echo "it kept running"
+# make sure the Gh pages repo is there and in the right branch
+pushd $GHPAGESDIR
 git checkout gh-pages
-rm -rf build/*
-git merge master
+popd
+
+# make the docs
 make html
-git add *
-git commit -a -m "Updating presentation materials"
+# copy to other repo (on the gh-pages branch)
+cp -R build/html/ $GHPAGESDIR
+
+pushd $GHPAGESDIR
+git add . # in case there are new files added
+git commit -a -m "updating presentation materials"
 git pull -s ours --no-edit
 git push
+

6108af469d0e2b57759fb54482ef28de18ba93b9
Chris Barker <PythonCHB@gmail.com>
2017-04-17 22:40:38 -0700

    updated capitalize code

--- Examples/capitalize/capital_mod.py
+++ Examples/capitalize/capital_mod.py
@@ -4,17 +4,65 @@
 A really simple module, just to demonstrate packaging
 """
 
-def capitalize_line(instr):
+from pathlib import Path
+
+
+def load_special_words(data_file_name, words=None):
+    """
+    Loads the special words (those that don't get capitalized)
+        
+    from the data file in the package
+
+    data file is a text file with one work per line
+    the # charactor is a comment -- everything after it will be ignored
+
+    """
+    words  = set() if words is None else words
+    with open(data_file_name) as data_file:
+        for line in data_file:
+            word = line.split('#')[0].strip()
+            if word:
+                words.add(word.lower())
+    return words
+
+def get_datafile_name():
+    """
+    return the default data file that comes with the package
+    """
+    return Path(__file__).parent / "cap_data.txt"
+
+## load up the special words on import
+special_words = load_special_words(get_datafile_name())
+
+print(special_words)
+
+
+
+def capitalize_line(instr, special_words=special_words):
     """
     capitalizes the input string
 
-    :param instr: the string to capitalize it should be a single line.
+    :param instr: the string to capitalize it should be a single sentence.
     :type instr: string
+
+    :param special_words: set of words that should not be capitalized
+                          defaults to the words in the encosed data file
+    :type special_words: set of str
 
     :returns: a capitalized version of instr
     """
+    new_words = []
+    for word in instr.split():
+        new = word.capitalize() if word not in special_words else word
+        new_words.append(new)
+    # capitalize the first word:
+    
+    if new_words:
+        new_words[0] = new_words[0].capitalize()
+    return " ".join(new_words)
 
-    return " ".join( word.capitalize() for word in instr.split() )
+    # return " ".join(word.capitalize() for word in instr.split()
+    #                 if word not in special_words)
 
 
 def capitalize(infilename, outfilename):
@@ -45,16 +93,3 @@ def capitalize(infilename, outfilename):
 
     return None
 
-
-class Something(object):
-    """
-    this is the class docstring
-    """
-    def __init__(self, input):
-        """
-        the __init__ docstring
-
-        :param input: someinput
-        :type input: some_type
-        """
-        pass
